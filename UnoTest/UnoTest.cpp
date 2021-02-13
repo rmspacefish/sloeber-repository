357fe597ef7bdead73dbf65be251a25747ed8bf9
Robin Mueller <robin.mueller.m@gmail.com>
2021-02-13 13:46:30 +0100

    init branch

---
+++ UnoTest/UnoTest.cpp
@@ -0,0 +1,25 @@
+#include "UnoTest.h"
+
+
+uint32_t currentPinState = LOW;
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+	Serial.begin(115200);
+	Serial.println("Serial port setup successfully");
+}
+
+void loop() {
+	if(currentPinState == LOW) {
+		Serial.println("Setting LED high");
+		digitalWrite(LED_BUILTIN, HIGH);
+		currentPinState = HIGH;
+	}
+	else {
+		Serial.println("Setting LED low");
+		digitalWrite(LED_BUILTIN, LOW);
+		currentPinState = LOW;
+	}
+	delay(2000);
+}
