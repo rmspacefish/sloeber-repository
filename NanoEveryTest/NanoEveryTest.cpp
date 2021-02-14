f571b99bd2ae31eb46c10a84a5bd3b4b9f7318ea
Robin.Mueller <robin.mueller.m@gmail.com>
2021-02-15 00:59:10 +0100

    added nano every test

---
+++ NanoEveryTest/NanoEveryTest.cpp
@@ -0,0 +1,24 @@
+#include "NanoEveryTest.h"
+
+uint32_t currentPinState = LOW;
+
+void setup() {
+	pinMode(LED_BUILTIN, OUTPUT);
+	digitalWrite(LED_BUILTIN, LOW);
+	Serial.begin(115200);
+	Serial.println("Serial port setup successfully");
+}
+
+void loop() {
+	if(currentPinState == LOW) {
+		Serial.println("Setting LED high");
+		digitalWrite(LED_BUILTIN, HIGH);
+		currentPinState = HIGH;
+	}
+	else {
+		Serial.println("Setting LED low");
+		digitalWrite(LED_BUILTIN, LOW);
+		currentPinState = LOW;
+	}
+	delay(2000);
+}
---
+++ NanoEveryTest/NanoEveryTest.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _NanoEveryTest_H_
+#define _NanoEveryTest_H_
+#include "Arduino.h"
+//add your includes for the project NanoEveryTest here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project NanoEveryTest here
+
+
+
+
+//Do not add code below this line
+#endif /* _NanoEveryTest_H_ */
